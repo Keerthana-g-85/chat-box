202a57c0e1b5c236a28aeb35a24a464c316a0d7f
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-05-10 15:40:53 +0530

    Backend socket server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('User connected')
+  console.log(socket.id)
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
+
+server.listen(3001, () => {
+  console.log('Server running on port 3001')
+})
