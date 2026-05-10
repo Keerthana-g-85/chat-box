63f8dc1c2c25ca299178290f63ddf7d3625f8fdc
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-05-10 15:45:01 +0530

    Setup chat state and socket connection

--- client/chat-box/src/pages/ChatPage.jsx
+++ client/chat-box/src/pages/ChatPage.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3001')
+
 export default function ChatPage() {
+
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState('')
+  const [joined, setJoined] = useState(false)
+
   return (
     <div>
       Chat Page
